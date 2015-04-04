20e277e6293b046f29e64a72fa472ad4933594c2
Arun <arunvir86@gmail.com>
2015-04-04 11:27:00 -0400

    change home page

--- TestMVCapplication1/App_Start/RouteConfig.cs
+++ TestMVCapplication1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TestMVCapplication1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Parent", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
